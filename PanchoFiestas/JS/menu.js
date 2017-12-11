193ef0ac90fdc6d19e340a09daa75cd112ac2caf
tamyt <tamitugues@gmail.com>
2017-12-11 11:11:50 -0300

    jquery menu

--- PanchoFiestas/JS/menu.js
+++ PanchoFiestas/JS/menu.js
@@ -1,20 +1,8 @@
 
-
-var menu = document.querySelector('.hamburger');
-
-function toggleMenu (event) {
-  this.classList.toggle('is-active');
-  document.querySelector( ".menuppal" ).classList.toggle("is_active");
-  event.preventDefault();
-}
-
-menu.addEventListener('click', toggleMenu, false);
-
-//Solución con jQUery
-/*$(document).ready(function(){
+$(document).ready(function(){
 	$('.hamburger').click(function() {
 		$('.hamburger').toggleClass('is-active');
-		$('.menuresponsive').toggleClass('is-active');
+		$('.menuppal').toggleClass('is_active');
 		return false;
 	});
-});*/
+});
